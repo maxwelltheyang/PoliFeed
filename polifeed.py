694b4f7b814f2cb0700da102eb6843496aaddae0
Maxwell Yang <maxwellbyang04@gmail.com>
2024-03-18 22:35:23 -0500

    changes

--- polifeed.py
+++ polifeed.py
@@ -1,9 +1,14 @@
 import requests
+from dotenv import load_dotenv
+import os
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
 def get_headline():
     base_url = "https://newsapi.org/v2/top-headlines"
     params = {
         'country': 'us',
-        'apiKey': '77f3e15e751b42468479f3de9027f77b'
+        'apiKey': API_KEY
     }
     response = requests.get(base_url, params=params)
     return response.json()
